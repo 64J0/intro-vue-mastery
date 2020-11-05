5368ada78dca2f660009f1a515e2a2fd5e61a930
gajo <vinigaio97@gmail.com>
2020-11-05 07:15:31 -0300

    Finalizandoa te a aula 9

--- main.js
+++ main.js
@@ -1,19 +1,93 @@
+Vue.component('product', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+      default: false
+    }
+  },
+  template: `
+  <div class="product">
+
+      <div class="product-image">
+        <img v-bind:src="image" :alt="description">
+      </div>
+
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p v-show="inStock">In Stock</p>
+        <p v-show="!inStock" style="text-decoration: line-through">Out of Stock</p>
+        <p>Shipping: {{ shipping }}</p>
+
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+
+        <div v-for="(variant, index) in variants" :key="variant.variantId" class="color-box"
+          :style="{ backgroundColor: variant.variantCollor, cursor: 'pointer' }" @mouseover="updateProduct(index)">
+        </div>
+
+        <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Add to Cart</button>
+
+      </div>
+
+    </div>
+    `,
+  data() {
+    return {
+      brand: 'Vue Mastery',
+      product: 'Socks',
+      selectedVariant: 0,
+      description: 'A pair o socks image',
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      variants: [
+        {
+          variantId: 2234,
+          variantCollor: "green",
+          variantImage: './assets/vmSocks-green-onWhite.jpg',
+          variantQuantity: 10
+        }, {
+          variantId: 2235,
+          variantCollor: "blue",
+          variantImage: './assets/vmSocks-blue-onWhite.jpg',
+          variantQuantity: 0
+        }
+      ]
+    }
+  },
+  methods: {
+    addToCart() {
+      this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
+    },
+    updateProduct(index) {
+      this.selectedVariant = index;
+    }
+  },
+  computed: {
+    title() {
+      return `${this.brand} - ${this.product}`;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    shipping() {
+      return this.premium ? "Free" : 2.99;
+    }
+  }
+});
+
 let app = new Vue({
   el: '#app',
   data: {
-    product: 'Boots',
-    image: './assets/vmSocks-green-onWhite.jpg',
-    description: 'A pair o socks image',
-    inStock: true,
-    details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    variants: [
-      {
-        variantId: 2234,
-        variantCollor: "green"
-      }, {
-        variantId: 2235,
-        variantCollor: "blue"
-      }
-    ]
+    premium: true,
+    cart: []
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
+    }
   }
 });
