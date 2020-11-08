1e8e4a54eb2b63c828b601f0bb986838abb9604c
gajo <vinigaio97@gmail.com>
2020-11-08 08:46:36 -0300

    Finishing the course updating the README.md

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Intro to Vue 2 - Vue Mastery
 
-In this course i'll develop an application that shows a product page with some interface events, like when hovering a specific button the image of the product will change and when you click in a add to cart button the total amount will increase (if the product is in stock).
+In this course I'll develop an application that shows a product page with some interface events, like when hovering a specific button the image of the product will change and when you click in a add to cart button the total amount will increase (if the product is in stock).
+
+This course uses the Vuejs in version 2 to teach the fundamentals of this tool. If you want to see the lessons just go to https://www.vuemastery.com.
 
 ---
 Vinícius Gajo Marques Oliveira, 2020.
--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// enables Vue in devtools
+Vue.config.devtools = true;
+
+let eventBus = new Vue();
+
 Vue.component('product', {
   props: {
     premium: {
@@ -29,7 +34,11 @@ Vue.component('product', {
 
         <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Add to Cart</button>
 
+        <button v-on:click="removeFromCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Remove from Cart</button>
+
       </div>
+
+      <product-tabs :reviews="reviews"></product-tabs>
 
     </div>
     `,
@@ -52,7 +61,8 @@ Vue.component('product', {
           variantImage: './assets/vmSocks-blue-onWhite.jpg',
           variantQuantity: 0
         }
-      ]
+      ],
+      reviews: []
     }
   },
   methods: {
@@ -61,6 +71,9 @@ Vue.component('product', {
     },
     updateProduct(index) {
       this.selectedVariant = index;
+    },
+    removeFromCart() {
+      this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId);
     }
   },
   computed: {
@@ -76,8 +89,116 @@ Vue.component('product', {
     shipping() {
       return this.premium ? "Free" : 2.99;
     }
+  },
+  mounted() {
+    eventBus.$on('review-submitted', productReview => {
+      this.reviews.push(productReview);
+    });
   }
 });
+
+Vue.component('product-review', {
+  template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+      
+      <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="error in errors">
+            {{ error }}
+          </li>
+        </ul>
+      </p>
+    
+      <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name" placeholder="name">
+      </p>
+      
+      <p>
+        <label for="review">Review:</label>      
+        <textarea id="review" v-model="review"></textarea>
+      </p>
+      
+      <p>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
+          
+      <p>
+        <input type="submit" value="Submit">  
+      </p>    
+
+    </form>
+  `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+      errors: []
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (this.name && this.review && this.rating) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating
+        }
+        eventBus.$emit('review-submitted', productReview);
+        this.name = null;
+        this.review = null;
+        this.rating = null;
+      } else {
+        if (!this.name) this.errors.push("Name required");
+        if (!this.review) this.errors.push("Review required");
+        if (!this.rating) this.errors.push("Rating required");
+      }
+    }
+  }
+});
+
+Vue.component('product-tabs', {
+  props: {
+    reviews: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `
+    <div>
+      <span class="tab" :class="{ activeTab: selectedTab === tab }" v-for="(tab, index) in tabs" :key="index" @click="selectedTab = tab">{{ tab }}</span>
+
+      <div v-show="selectedTab === 'Reviews'">
+        <h2>Reviews</h2>
+        <p v-if="!reviews.length">There are no reviews yet.</p>
+        <ul>
+          <li v-for="review in reviews">
+            <p>{{ review.name }}</p>
+            <p>Rating: {{ review.rating }}</p>
+            <p>{{ review.review }}</p>
+          </li>
+        </ul>
+      </div>
+
+      <product-review v-show="selectedTab === 'Make a Review'"></product-review>
+    </div>
+  `,
+  data() {
+    return {
+      tabs: ['Reviews', 'Make a Review'],
+      selectedTab: 'Reviews'
+    }
+  }
+})
 
 let app = new Vue({
   el: '#app',
@@ -88,6 +209,12 @@ let app = new Vue({
   methods: {
     updateCart(id) {
       this.cart.push(id);
+    },
+    removeItem(id) {
+      const index = this.cart.indexOf(id);
+      const half_1 = this.cart.slice(0, index);
+      const half_2 = this.cart.slice(index + 1);
+      this.cart = new Array().concat(half_1, half_2);
     }
   }
 });
